1b152d3cda16b0f10afb93d6b542f291c9deb7c2
lucasgarcez <lucasgarcezdev@gmail.com>
2020-05-09 09:48:19 -0300

    inital routes and styled configs

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+const Dashboard: React.FC = () => {
+  return <Title>Explorar Repositórios</Title>;
+};
+
+export default Dashboard;
