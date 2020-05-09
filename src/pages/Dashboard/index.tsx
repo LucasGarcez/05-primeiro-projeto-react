09a123dbc345d08da5ebf9ecfb44bfe576d28f02
lucasgarcez <lucasgarcezdev@gmail.com>
2020-05-09 11:00:41 -0300

    style repo item

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import { Title, Form, Repositories } from './styles';
+import logo from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explorar Repositórios</Title>;
+  return (
+    <>
+      <img src={logo} alt="Git Explorer" />
+      <Title>Explore epositórios no Github</Title>
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/12939735?s=460&u=e4960b838d3efbf6d617329d6f6240eb2dbe5795&v=4"
+            alt="Lucas Garcez"
+          />
+          <div>
+            <strong>Lucas Repo</strong>
+            <p>Descrição do repositório</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
