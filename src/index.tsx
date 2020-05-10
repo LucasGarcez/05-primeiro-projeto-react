b40f31486c80bd392b2da2d6a2f565abeb558a65
lucasgarcez <lucasgarcezdev@gmail.com>
2020-05-09 23:07:21 -0300

    inital config apollo-boost

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient, { gql } from 'apollo-boost';
+
 import App from './App';
+import { token } from './config/graphql';
+
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+});
+
+client
+  .query({
+    query: gql`
+      {
+        repository(owner: "facebook", name: "react") {
+          id
+          nameWithOwner
+          description
+          owner {
+            login
+            avatarUrl
+          }
+        }
+      }
+    `,
+  })
+  .then((result) => console.log('connected Github API', result));
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
-
